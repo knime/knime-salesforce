b312c192d645cf3aad558a59b829d7ee994637f7
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2020-07-05 22:38:20 +0200

    AP-11268: added Salesforce Connection Extractor node
    AP-11268 (Salesforce Reader (multiple nodes, e.g. Salesforce OAuth2
    Authentication, SOQL Query, ...))

---
+++ org.knime.salesforce/src/org/knime/salesforce/extractor/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A node that extracts access token and instance URL from a SalesforceConnectPortObjectSpec.
+ * @author Bernd Wiswedel, KNIME GmbH, Konstanz, Germany
+ */
+package org.knime.salesforce.extractor;
